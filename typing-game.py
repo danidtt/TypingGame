85b78a6d2f6f703241f04724f045c776681548f0
Daniella Dantas <71736448+danidtt@users.noreply.github.com>
2021-06-03 15:20:47 -0300

    Add files via upload

---
+++ typing-game.py
@@ -0,0 +1,30 @@
+# Ideia: Jogo competitivo de quem digita mais rápido
+
+# No terminal, instale requests com o comando: pip install requests
+
+import requests
+import random
+import time
+
+url = 'http://www.mit.edu/~ecprice/wordlist.10000'
+
+resposta = requests.get(url)
+palavras = resposta.content.splitlines()
+
+palavras = [palavra.decode('utf-8') for palavra in palavras]
+
+randomWords = random.sample(palavras, 10)
+
+pontos = 0
+tic = time.perf_counter()
+
+for palavra in randomWords:
+    print(palavra)
+    entrada = input()
+    if entrada == palavra:
+        pontos = pontos + 1
+
+tac = time.perf_counter()
+
+print(pontos)
+print(tac-tic)
